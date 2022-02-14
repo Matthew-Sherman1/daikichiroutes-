762c5a985ac45ea34d4899da308b083cddf563eb
Matthew-Sherman1 <93622077+Matthew-Sherman1@users.noreply.github.com>
2022-02-14 11:34:06 -0700

    first commit

--- src/main/java/com/matthewsherman/daikichiroutes/daikichicontroller.java
+++ src/main/java/com/matthewsherman/daikichiroutes/daikichicontroller.java
@@ -1,5 +1,6 @@
 package com.matthewsherman.daikichiroutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +25,16 @@ public class daikichicontroller {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 	}
 	
-	
-
+	@RequestMapping("/travel/{location}")
+	public String travel(@PathVariable("location")  String location) {
+		
+		return "Congratulations! You will soon travel to " + location;
+		}
+	@RequestMapping("/lotto/{num}")
+	public String lotto(@PathVariable("num")  int num) {
+		if(num % 2 ==0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers";
+		}
+		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
 }
